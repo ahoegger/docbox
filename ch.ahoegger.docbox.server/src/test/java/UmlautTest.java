ce7c4b4eef2f58d7955ae6834bb403a4d30afa8e
Andreas Hoegger <github@ahoegger.ch>
2016-04-04 15:34:42 +0200

    Change-Id: I0a855a796312cd9ace66c27c1ba361267fad60ad

--- ch.ahoegger.docbox.server/src/test/java/UmlautTest.java
+++ ch.ahoegger.docbox.server/src/test/java/UmlautTest.java
@@ -17,6 +17,7 @@ public class UmlautTest {
 //    for (Entry<Object, Object> e : System.getProperties().entrySet()) {
 //      System.out.println(e.getKey() + " - " + e.getValue());
 //    }
+    LOG.warn("USER_PROP: " + System.getProperty("user.prop1"));
     LOG.warn("SYS PROP: " + System.getProperty("file.encoding"));
     LOG.warn("CHARSET: " + Charset.defaultCharset());
     String a = "öäöü";
@@ -26,5 +27,11 @@ public class UmlautTest {
 
     s = new String(a.getBytes(), Charset.forName("UTF-8"));
     LOG.warn("second: " + s);
+
+    System.setProperty("file.encoding", "UTF-8");
+    String b = "äöü";
+    s = new String(a.getBytes(), Charset.forName("UTF-8"));
+    LOG.warn("second[b]: " + b);
   }
+
 }
